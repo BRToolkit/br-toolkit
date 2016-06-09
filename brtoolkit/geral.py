1b19f2212c0b16a0630ac16f70c0c55771b84673
Vinicius Mesel (@vmesel) <vinicius@bigsp.com.br>
2016-06-09 20:48:14 -0300

    Nova função sob desenvolvimento

--- brtoolkit/geral.py
+++ brtoolkit/geral.py
@@ -1,10 +1,13 @@
+from nltk import word_tokenize
 import string
 import collections
 
 class Geral():
 
-    def removedor_pontuacao(self, text):
-
+    def removedor_pontuacao(text):
+        """ Método que remove a pontuação da frase
+        para poder retornar um array com todas as
+        palavras de forma limpa """
         _txt = text.split(" ")
         _neotexto = []
         for _word in _txt:
@@ -14,7 +17,14 @@ class Geral():
 
         return(_neotexto)
 
-    def conta_palavras(self, text):
-        texto = Geral().removedor_pontuacao(text)
-        contas = collections.Counter(texto)
-        return(contas)
+    def conta_palavras(text):
+        return(collections.Counter(Geral().removedor_pontuacao(text)))
+
+    def diff_textos(texto1, texto2):
+        try:
+            leitura1 = open(texto1, "r")
+            leitura2 = open(texto2, "r")
+
+
+        except Except:
+            return "Houve um problema na leitura de um dos seus arquivos"
