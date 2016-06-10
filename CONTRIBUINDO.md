b1e91d2b269e704264ad801a74845553a78cf59b
Vinicius Mesel (@vmesel) <vinicius@bigsp.com.br>
2016-06-09 23:02:32 -0300

    Algumas mods!

---
+++ CONTRIBUINDO.md
@@ -0,0 +1,6 @@
+# Contribua para o projeto
+
+Para contribuir com o BR-Toolkit deve se manter a estrutura do projeto, causando a menor deformidade o possível, permitindo que todos tenham o entendimento pleno sobre sua alteração. Para isso a estrutura do projeto deve se manter nos arquivos:
+- Números
+- palavras
+- Textos
--- brtoolkit/geral.py
+++ brtoolkit/geral.py
@@ -1,30 +1,36 @@
 from nltk import word_tokenize
 import string
 import collections
-
+from re import compile
 class Geral():
 
-    def removedor_pontuacao(text):
+    def palavras(text):
         """ Método que remove a pontuação da frase
         para poder retornar um array com todas as
         palavras de forma limpa """
-        _txt = text.split(" ")
-        _neotexto = []
-        for _word in _txt:
-            for ponto in string.punctuation :
-                _word = _word.replace(ponto, "")
-            _neotexto.append(_word)
 
-        return(_neotexto)
+        regex = compile("\w+")
+        saida = regex.findall(text)
+        return " ".join(saida)
 
     def conta_palavras(text):
-        return(collections.Counter(Geral().removedor_pontuacao(text)))
+        """ Função que retorna a contagem de todas
+        as palavras de um texto selecionado pelo
+        usuário"""
+
+        return(collections.Counter(
+        Geral.palavras(text).split(" ")
+        ))
 
     def diff_textos(texto1, texto2):
+        """ Função que retorna o diferencial entre
+        dois textos selecionados e inputados pelo
+        usuario do BR-Toolkit """
         try:
             leitura1 = open(texto1, "r")
             leitura2 = open(texto2, "r")
 
 
         except Except:
-            return "Houve um problema na leitura de um dos seus arquivos"
+            return "Houve um problema na leitura de um \
+            dos seus arquivos"
