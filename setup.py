416be4fca450ffea2e8f74170d83108c090715d6
Vinicius Mesel (@vmesel) <vinicius@bigsp.com.br>
2016-08-07 18:55:02 -0300

    Separador de Silabas adicionado

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name='BR Toolkit',
-      version='0.0.1',
+      version='0.0.1.1',
       description='Ferramenta para análise de Português Brasileiro',
       author='BR Toolkit',
       author_email='mendesxeduardo@gmail.com',
-      url='https://github.com/z4r4tu5tr4/br-toolkit',
+      url='https://github.com/BRToolkit/br-toolkit',
       packages=['natural language procesing', 'portuguese processing', "words processing", "processador de textos"],
      )
